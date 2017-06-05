484c7cb3d5e85a29fb24030d49d976648127c8ea
Paula Ponce <paula.poncez@usach.cl>
2017-06-05 18:30:09 -0400

    esquemas usando la propiedad position con DOM

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var caja1_1 = document.getElementsByClassName("caja-1")[0];
+var caja2_1 = document.getElementsByClassName("caja-2")[0];
+var caja3_1 = document.getElementsByClassName("caja-3")[0];
+
+caja1_1.style.width = "200px";
+caja1_1.style.height = "200px";
+caja1_1.style.margin = "10px";
+
+caja2_1.style.width = "100px";
+caja2_1.style.height = "100px";
+
+caja3_1.style.width = "50px";
+caja3_1.style.height = "50px";
+
+var rojo = document.getElementById("rojo");
+var morado = document.getElementById("morado");
+var amarillo = document.getElementById("amarillo");
+
+rojo.style.background = "#FB030F";
+rojo.style.position = "relative";
+morado.style.background = "#9C01FF";
+morado.style.position = "absolute";
+morado.style.top = "50px";
+morado.style.right = "50px";
+amarillo.style.background = "#FBF201";
+amarillo.style.position = "absolute";
+
+var caja1_2 = document.getElementsByClassName("caja-1")[1];
+var caja2_2 = document.getElementsByClassName("caja-2")[1];
+var caja3_2 = document.getElementsByClassName("caja-3")[1];
+
+caja1_2.style.width = "200px";
+caja1_2.style.height = "200px";
+caja1_2.style.margin = "10px";
+
+caja2_2.style.width = "100px";
+caja2_2.style.height = "100px";
+
+caja3_2.style.width = "50px";
+caja3_2.style.height = "50px";
+
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var negro = document.getElementById("negro");
+
+azul.style.background = "#1A02FC";
+azul.style.position = "relative";
+verde.style.background = "#198F06";
+verde.style.position = "absolute";
+verde.style.top = "50px";
+verde.style.right = "50px";
+negro.style.background = "#050505";
+negro.style.position = "absolute";
